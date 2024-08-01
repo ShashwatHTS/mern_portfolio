69d7a4b164583752d4ae99aa698efdc49bfb1630
shashwatHTS <shashwatpal@htstechsolutions.com>
2024-08-01 15:50:25 +0530

    header: completed

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div className="p-5 bg-primary flex justify-center">
+      <h1 className="text-secondary text-4xl font-semibold">Shashwat Pal</h1>
+    </div>
+  )
+}
+
+export default Header
