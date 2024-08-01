e743f2487ef19cdd067ed784f2e4d06ff09b042f
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-08-01 16:26:02 +0530

    aaded margin m-10

--- frontend/src/pages/Home/Intro.jsx
+++ frontend/src/pages/Home/Intro.jsx
@@ -1,14 +1,14 @@
 
 const Intro = () => {
   return (
-    <div className="h-[80vh] bg-primary flex flex-col items-start justify-center gap-8">
+    <div className="h-[80vh] bg-primary flex flex-col items-start justify-center gap-8 my-10">
       <h1 className="text-white">Hi , I am </h1>
       <h1 className="text-7xl text-secondary font-semibold">Shashwat Pal</h1>
       <h1 className="text-7xl text-white font-semibold">I am a Full Stack Developer</h1>
       <p className="text text-white w-2/3">
         I am a Full Stack Developer. Currently working as a Software Developer at HTS Tech Solutions pvt. Ltd. Having a skills in ReactJS, NextJS, NodeJS, ExpresJs MongoDB, PostgreSQL, Supabase, Docker, AWS.
       </p>
-      
+
       <button className="border-2 border-tertiary text-tertiary px-10 py-3 rounded">Get Started</button>
     </div>
 
