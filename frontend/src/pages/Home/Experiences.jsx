76c74bc3e83ce070929faf2adeaf4a2d7168dbbd
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-10-15 11:40:58 +0530

    added: experience date

--- frontend/src/pages/Home/Experiences.jsx
+++ frontend/src/pages/Home/Experiences.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import SectionTitle from '../../components/SectionTitle'
+import { experiences } from '../../resources/experiences'
 
 const Experiences = () => {
+  const [selectedItemIndex, setSelectedItemIndex] = React.useState(0)
   return (
     <div>
       <SectionTitle title="Experiences" />
+
+      <div className='flex py-10'>
+        <div className='flex flex-col gap-10 border-l-4 border-[#135e4c82]'>
+          {experiences.map((experience, index) => (
+            <div onClick={() => setSelectedItemIndex(index)} className='cursor-pointer'>
+              <h1 className={`text-xl px-5 ${selectedItemIndex === index ? 'text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#1a7f5a5f] ' : 'text-white'} `}>{experience.date}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
